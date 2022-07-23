b5d6496ca45123739a8a6cccf3b422a154448ab9
eunji0123 <helloeunji@hafs.kr>
2022-07-24 00:25:27 +0900

    Using Jpa

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,6 +1,14 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // 이 annotation을 붙이면 Member는 jpa가 관리하는 Entity가 됨.
 public class Member {
+    @Id // Primary Key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Member객체를 insert 하면 PK인 id는 데이터베이스가 알아서 생성
     private Long id;
     private String name;
 
